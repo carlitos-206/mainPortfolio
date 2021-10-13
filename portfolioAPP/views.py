87e1e14c051455cad46ceab657673be27f0a7f9e
carlitos-206 <carloscaceres041@gmail.com>
2021-10-12 18:09:22 -0700

    new breakpoints

--- portfolioAPP/views.py
+++ portfolioAPP/views.py
@@ -3,5 +3,3 @@ from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
     return render(request, "index.html")
-def resume(request):
-    return render(request, "Resume.html")
